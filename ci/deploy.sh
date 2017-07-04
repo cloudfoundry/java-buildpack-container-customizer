1adaa3353f6b9a28c28033928bd8226e9b0564ae
Ben Hale <bhale@pivotal.io>
2017-07-04 14:12:26 +0100

    Simpler Concourse Caching

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,4 +3,4 @@
 set -e -u
 
 cd java-buildpack-container-customizer
-./mvnw -q -Dmaven.repo.local=../m2/repository -Dmaven.user.home=../m2 -Dmaven.test.skip=true deploy
+./mvnw -q -Dmaven.test.skip=true deploy
