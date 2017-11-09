8c71d38325f0185ece56e6647d479bdf3efc0f39
Ben Hale <bhale@pivotal.io>
2017-11-09 11:59:19 -0800

    Support Spring Boot 2
    Previously, this project made use of an API for customizing the embedded
    container that was removed in Spring Boot 2.  This change updates the code to
    provide implementations for both Spring Boot 1 and Spring Boot 2 and some
    fancy JAR shading to get them both to coexist with one another.

---
+++ java-buildpack-container-customizer-boot-1/src/main/java/org/cloudfoundry/container/customizer/boot1/AllowLinkingEmbeddedServletContainerCustomizer.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2016-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cloudfoundry.container.customizer.boot1;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.core.Ordered;
+
+import java.util.logging.Logger;
+
+final class AllowLinkingEmbeddedServletContainerCustomizer implements EmbeddedServletContainerCustomizer, Ordered {
+
+    private final Logger logger = Logger.getLogger(getClass().getName());
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        if (!TomcatEmbeddedServletContainerFactory.class.isAssignableFrom(container.getClass())) {
+            this.logger.warning("This is not a Tomcat container. Symbolically linked files will not be enabled for this application.");
+            return;
+        }
+
+        ((TomcatEmbeddedServletContainerFactory) container).addContextCustomizers(context -> context.getResources().setAllowLinking(true));
+    }
+
+    @Override
+    public int getOrder() {
+        return HIGHEST_PRECEDENCE;
+    }
+
+}
---
+++ java-buildpack-container-customizer-boot-1/src/test/java/org/cloudfoundry/container/customizer/boot1/AllowLinkingEmbeddedServletContainerCustomizerTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2016-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cloudfoundry.container.customizer.boot1;
+
+import org.junit.Test;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+public final class AllowLinkingEmbeddedServletContainerCustomizerTest {
+
+    @Test
+    public void customize() {
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+
+        new AllowLinkingEmbeddedServletContainerCustomizer().customize(factory);
+
+        assertThat(factory.getTomcatContextCustomizers()).hasSize(1);
+    }
+
+    @Test
+    public void customizeNonTomcat() {
+        ConfigurableEmbeddedServletContainer factory = mock(ConfigurableEmbeddedServletContainer.class);
+
+        new AllowLinkingEmbeddedServletContainerCustomizer().customize(factory);
+    }
+
+}
